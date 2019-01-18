ed205f0a9a85eb67d21cfd5695143f9d42ed3b44
ruanchaves93 <ruanchaves93@gmail.com>
2019-01-18 09:54:09 -0200

    test key

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, Response
 from flask_httpauth import HTTPBasicAuth
+from utils import Driver, Listener
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 users = {
-    'admin' : 'admin'
+    'prawstreams' : 'prawstreams'
 }
 
 @auth.get_password
@@ -14,11 +15,22 @@ def get_pw(username):
         return users.get(username)
     return None
 
-@app.route('/')
+@app.route('/auth')
 @auth.login_required
 def auth():
     database_key = os.environ.get('DATABASE_URL')
     return database_key
+
+@app.route('/')
+def fetch():
+    def listen():
+        listener = Listener()
+        listener.connect(mode='heroku')
+        for item in listener:
+            yield item
+    return Response(listen(),mimetype='json')
+
+
 
 if __name__ == '__main__':
     app.run()
