69da0989aae1dafd03539433e4d879feab691b50
ruanchaves93 <ruanchaves93@gmail.com>
2019-01-18 10:35:46 -0200

    go there

--- app.py
+++ app.py
@@ -31,32 +31,12 @@ def fetch():
             yield json.dumps(item)
     return Response(listen(),mimetype='json')
 
-
+@app.route('/accounts')
+def accounts():
+    driver = Driver()
+    driver.connect(mode='heroku')
+    result = driver.pull('select * from accounts')
+    return json.dumps(result)
 
 if __name__ == '__main__':
     app.run()
-# from utils import Driver, Listener
-# import psycopg2
-
-# from flask import Flask
-# from flask_httpauth import HTTPBasicAuth
-# app = Flask(__name__)
-
-# def get_users():
-#     driver = Driver()
-#     driver.connect(mode='heroku')
-
-# @app.route("/")
-# def home():
-#     return 'hello world'
-#     # def listen():
-#     #     listener = Listener()
-#     #     listener.connect(mode='heroku')
-#     #     for item in listen:
-#     #         yield item
-#     #     return Response(listen(), mimetype='json')
-
-# @app.route("/auth")
-# @auth.login_required
-# def auth():
-#     return os.environ['DATABASE_URL']
